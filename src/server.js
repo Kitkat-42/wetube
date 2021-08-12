2e7cc948aa310f94dba0f92ea76381c0acb8a9b6
Seoyoung Jeong <issyo1022@gmail.com>
2021-08-12 21:08:50 +0900

    feat: add morgan

--- src/server.js
+++ src/server.js
@@ -1,28 +1,23 @@
 import express from "express";
-const PORT = 4000;
-const app = express();
+import morgan from "morgan";
 
-const routerLogger = (req, res, next) => {
-  console.log("PATH", req.path);
-  next();
-};
-const methodLogger = (req, res, next) => {
-  console.log("METHOD", req.method);
-  next();
-};
+const PORT = 4000;
+
+const app = express();
+const logger = morgan("dev");
+
 const home = (req, res) => {
-  console.log("I will respond.");
-  res.send("hello");
+  return res.send("hello");
 };
 const login = (req, res) => {
   return res.send("login");
 };
 
-app.use(methodLogger, routerLogger); // 위에 있어야 "/", "/login" 모두에 적용이 됨
+app.use(logger);
 app.get("/", home);
 app.get("/login", login);
 
 const handleListening = () =>
-  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
