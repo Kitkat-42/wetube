37ba8b5aab1f04d8fa3adcd19a9840b0ca2449ac
Seoyoung Jeong <issyo1022@gmail.com>
2021-08-12 22:01:21 +0900

    feat: add routers

--- src/server.js
+++ src/server.js
@@ -1,10 +1,19 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
+app.use(logger);
+
+/* 라우터 사용하기 */
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const home = (req, res) => {
   return res.send("hello");
@@ -12,10 +21,6 @@ const home = (req, res) => {
 const login = (req, res) => {
   return res.send("login");
 };
-
-app.use(logger);
-app.get("/", home);
-app.get("/login", login);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
